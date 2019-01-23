5be149756eefacb2a310b175bcec333127520368
Malcolm Ramsay <malramsay64@gmail.com>
2019-01-23 14:21:26 +1100

    Swap Alt and Gui on home row on Zeal60
    This swaps these two keys back to the standard Windows/Linux layout
    instead of the MacOS layout. This is more sensible as a default since it
    is easier to swap these keys on MacOS.

--- keyboards/zeal60/keymaps/malcolm/keymap.c
+++ keyboards/zeal60/keymaps/malcolm/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,  KC_BSPC},
         {MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_GRV},
         {KC_LSFT, _x_ ,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH ,  KC_RSFT,  _x_   },
-        {MO(1), KC_LALT, KC_LGUI, _x_, _x_, _x_, _x_,      LT(1, KC_SPC),      _x_, _x_, KC_RGUI,  KC_RALT,   MO(1),  KC_RCTRL  }
+        {MO(1), KC_LGUI, KC_LALT, _x_, _x_, _x_, _x_,      LT(1, KC_SPC),      _x_, _x_, KC_RALT,  KC_RGUI,   MO(1),  KC_RCTRL  }
     },
     /* 1: Fn layer
      * ,-----------------------------------------------------------.
