a3c78f236c36a3c8adb8b50780899e8872396485
Malcolm Ramsay <malramsay64@gmail.com>
2019-07-30 19:56:47 +1000

    Update board definitions for compatibility
    These are some compatibility updates to my board definitions

--- keyboards/satan/keymaps/malcolm/keymap.c
+++ keyboards/satan/keymaps/malcolm/keymap.c
@@ -20,12 +20,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Ctrl|Gui |Alt |      Space             |Alt |Fn4 |Fn4 |Fn1 |
      * `-----------------------------------------------------------'
      */
-    KEYMAP_HHKB(
+    LAYOUT_60_ansi_split_bs_rshift(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, \
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,  KC_BSPC, \
         MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,  \
         KC_LSFT,   KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH ,  KC_RSFT,  KC_NO, \
-        MO(1), KC_LALT, KC_LGUI,           LT(1, KC_SPC),                    KC_RGUI, KC_RALT, MO(1), KC_RCTRL),
+        MO(1), KC_LALT, KC_LGUI,           LT(1, KC_SPC),                    KC_RGUI, KC_RALT, MO(1), KC_RCTRL \
+    ),
     /* 1: Fn layer
      * ,-----------------------------------------------------------.
      * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|BLD|BLI|
@@ -39,22 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
      */
-    KEYMAP_HHKB(
-        ______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, F(2), F(3), \
-        KC_CAPS, ______, ______, ______, ______, ______, ______, KC_PGDN, KC_HOME, KC_PGUP, ______, ______,  F(4), KC_DEL, \
+    LAYOUT_60_ansi_split_bs_rshift(
+        ______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, BL_INC, BL_DEC, \
+        KC_CAPS, ______, ______, ______, ______, ______, ______, KC_PGDN, KC_HOME, KC_PGUP, ______, ______,  BL_TOGG, KC_DEL, \
         ______, ______, KC_VOLD, KC_VOLU, KC_MUTE, ______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, ______, ______, ______, \
         ______, KC_MPRV, KC_MPLY, KC_MNXT, ______, ______, KC_PPLS, KC_PMNS, KC_END, ______, ______,  ______, ______, \
         ______, ______, ______,                 ______,                    ______, ______, ______, ______),
 };
-
-/*
- * Fn action definition
- */
-const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(1),
-    [1] = ACTION_LAYER_TAP_KEY(MOD_LCTL, KC_ESC),
-    [2] = ACTION_BACKLIGHT_DECREASE(),
-    [3] = ACTION_BACKLIGHT_INCREASE(),
-    [4] = ACTION_BACKLIGHT_TOGGLE(),
-};
-
--- keyboards/whitefox/keymaps/malcolm/keymap.c
+++ keyboards/whitefox/keymaps/malcolm/keymap.c
@@ -32,14 +32,14 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Ctrl|Alt |Gui |         Space    |Fn0 |Gui |Alt |  |Lef|Dow|Rig|
      * `---------------------------------------------------------------'
      */
-    [0] = KEYMAP( \
+    [0] = LAYOUT( \
             KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_MUTE,\
             KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,      KC_DEL, \
             CTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,   KC_PGUP,\
             KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,\
             MO(1), KC_LGUI, KC_LALT,           LT(1, KC_SPC),      KC_RALT, KC_RGUI, KC_NUBS,   KC_LEFT, KC_DOWN, KC_RGHT \
             ),
-    [1] = KEYMAP( \
+    [1] = LAYOUT( \
             ______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, BL_DEC, BL_INC, KC_SCROLLLOCK, \
             KC_CAPS, ______, ______, ______, ______, ______, ______, KC_PGDN, KC_HOME, KC_PGUP, ______, ______, BL_TOGG, KC_DEL, KC_F14, \
             ______, ______, KC_VOLD, KC_VOLU, KC_MUTE, ______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, ______, ______, ______, ______, KC_VOLU, \
